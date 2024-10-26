7a89ecabf643be4c71d58601be0c5d4a590c23de
nilesh7272 <nileshparadva97@gmail.com>
2024-10-26 09:56:47 +0530

    React Employee Form

--- src/components/employeeform/forminput/FormInputCreate.jsx
+++ src/components/employeeform/forminput/FormInputCreate.jsx
@@ -5,7 +5,7 @@ const FormInputCreate = ({ inputBox, inputHandle, inputValid }) => {
                 {["Name", "Phone_number", "Address", "Email", "Designation", "Department", "City"].map((data) => (
                     <div className={`eployee-field ${data}`} key={data}>
                         <label htmlFor="" className="font-semibold text-1xl mb-1 text-white">{data + " :-"}</label>{inputValid[data]}
-                        <input type="text" placeholder={"Enter your " + data} name={data} value={inputBox[data]} className="w-full border-2 border-[#9f9797] py-2 px-3 bg-[#3e3c3c] rounded-lg mb-3 outline-none placeholder:text-white text-white" onChange={inputHandle} />
+                        <input type="text" placeholder={"Enter your " + data} name={data} value={inputBox[data]} className="w-full border-2 border-[#9f9797] py-2 px-3 bg-[#3e3c3c] rounded-lg mb-3 outline-none placeholder:text-[#a19d9d] text-white" onChange={inputHandle} />
                     </div>
                 ))}
             </form>
