5bd6cab0c1d303fdac97736f8579c4444f8e364a
nilesh7272 <nileshparadva97@gmail.com>
2024-10-28 10:50:54 +0530

    React Employee Form

--- src/components/employeeform/Employee.jsx
+++ src/components/employeeform/Employee.jsx
@@ -17,29 +17,29 @@ const Employee = () => {
         City: ''
     })
 
-    const [validField, setValidField] = useState({});
+    // const [validField, setValidField] = useState({});
     const [employeeStorage, setEmployeeStorage] = useState(StorageGet());
     const [IdMatch, setIdMatch] = useState(null);
 
-    const validError = (name, value) => {
-        const isValid = {
-            Name: /^[A-Za-z ]{2,}$/.test(value),
-            Phone_number: /^[0-9]{10}$/.test(value),
-            Address: /^[A-z -/]+\d+\s[A-z]+\s[A-z]+(.*)$/.test(value),
-            Email: /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/.test(value),
-            Designation: /^[A-Za-z ]{2,}$/.test(value),
-            Department: /^[A-Za-z ]{2,}$/.test(value),
-            City: /^[A-Za-z ]{2,}$/.test(value),
-        }
+    // const validError = (name, value) => {
+    //     const isValid = {
+    //         Name: /^[A-Za-z ]{2,}$/.test(value),
+    //         Phone_number: /^[0-9]{10}$/.test(value),
+    //         Address: /^[A-z -/]+\d+\s[A-z]+\s[A-z]+(.*)$/.test(value),
+    //         Email: /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/.test(value),
+    //         Designation: /^[A-Za-z ]{2,}$/.test(value),
+    //         Department: /^[A-Za-z ]{2,}$/.test(value),
+    //         City: /^[A-Za-z ]{2,}$/.test(value),
+    //     }
 
-        setValidField((prev) => ({ ...prev, [name]: isValid[name] ? (<span className="text-green-400 font-semibold ps-2">Valid {name}</span>) : (<span className="text-red-400 font-semibold ps-2">Invalid {name}</span>) }))
-        return isValid[name];
-    }
+    //     setValidField((prev) => ({ ...prev, [name]: isValid[name] ? (<span className="text-green-400 font-semibold ps-2">Valid {name}</span>) : (<span className="text-red-400 font-semibold ps-2">Invalid {name}</span>) }))
+    //     return isValid[name];
+    // }
 
     const handleField = (e) => {
         const { name, value } = e.target;
         setInputField({ ...inputField, [name]: value })
-        validError(name, value)
+        // validError(name, value)
     }
 
     const DataDelete = (id,e) => (setEmployeeStorage(employeeStorage.filter((data) => data.Id != id)))
@@ -51,20 +51,19 @@ const Employee = () => {
     const FormSubmit = (e) => {
         e.preventDefault();
 
-        const isValidForm = Object.keys(inputField).every(field => validError(field, inputField[field]));
-        if (!isValidForm) {
-            return alert("Form not submitted");
+        const isValidForm = Object.values(inputField).every(value => value.trim() !== '');
+        if (isValidForm) {
+            const randomId = generateUniqueId({ // const randomId = Math.floor(Math.random() * 10000);
+                length: 4,
+                useLetters: false
+            })
+
+            const dataToSubmit = { ...inputField, Id: IdMatch || randomId };
+            setEmployeeStorage(prev => IdMatch ? prev.map((data) => data.Id === IdMatch ? dataToSubmit : data) : [...prev, dataToSubmit]);
+            setInputField({ Name: '', Phone_number: '', Address: '', Email: '', Designation: '', Department: '', City: '' });
+        } else {
+            alert("Form not submitted");
         }
-
-        const randomId = generateUniqueId({ // const randomId = Math.floor(Math.random() * 10000);
-            length: 4,
-            useLetters: false
-        })
-
-        const dataToSubmit = { ...inputField, Id: IdMatch || randomId };
-        setEmployeeStorage(prev => IdMatch ? prev.map((data) => data.Id === IdMatch ? dataToSubmit : data) : [...prev, dataToSubmit]);
-        setInputField({Name: '', Phone_number: '', Address: '', Email: '', Designation: '', Department: '', City: ''});
-        setValidField({});
     }
 
     useEffect(() => {
@@ -78,7 +77,7 @@ const Employee = () => {
                     <div className="w-12/12 relative flex items-center justify-center flex-col bg-my-image bg-cover bg-top  py-5 rounded-lg overflow-hidden">
                         <div className="absolute inset-0 bg-black opacity-65"></div>
                         <div className="w-6/12 border-2 backdrop-blur-lg rounded-lg overflow-hidden">
-                            <FormInputCreate inputBox={inputField} inputHandle={handleField} inputValid={validField} />
+                            <FormInputCreate inputBox={inputField} inputHandle={handleField} /*inputValid={validField}*/ />
                             <SubmitButton FormSubmited={FormSubmit} />
                         </div>
                     </div>
