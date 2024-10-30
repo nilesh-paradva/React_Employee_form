35bfe30c0ebe860697ea522d7d0340b6ad1893f3
nilesh7272 <nileshparadva97@gmail.com>
2024-10-30 13:03:32 +0530

    React Employee Form

--- src/components/employeeform/Employee.jsx
+++ src/components/employeeform/Employee.jsx
@@ -17,33 +17,43 @@ const Employee = () => {
         City: ''
     })
 
-    // const [validField, setValidField] = useState({});
+    const [validField, setValidField] = useState({});
     const [employeeStorage, setEmployeeStorage] = useState(StorageGet());
     const [IdMatch, setIdMatch] = useState(null);
 
-    // const validError = (name, value) => {
-    //     const isValid = {
-    //         Name: /^[A-Za-z ]{2,}$/.test(value),
-    //         Phone_number: /^[0-9]{10}$/.test(value),
-    //         Address: /^[A-z -/]+\d+\s[A-z]+\s[A-z]+(.*)$/.test(value),
-    //         Email: /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/.test(value),
-    //         Designation: /^[A-Za-z ]{2,}$/.test(value),
-    //         Department: /^[A-Za-z ]{2,}$/.test(value),
-    //         City: /^[A-Za-z ]{2,}$/.test(value),
-    //     }
+    const validError = (name, value) => {
+        const isValid = {
+            Name: /^[A-Za-z ]{2,}$/.test(value),
+            Phone_number: /^[0-9]{10}$/.test(value),
+            Address: /^[A-z -/]+\d+\s[A-z]+\s[A-z]+(.*)$/.test(value),
+            Email: /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/.test(value),
+            Designation: /^[A-Za-z ]{2,}$/.test(value),
+            Department: /^[A-Za-z ]{2,}$/.test(value),
+            City: /^[A-Za-z ]{2,}$/.test(value),
+        }
 
-    //     setValidField((prev) => ({ ...prev, [name]: isValid[name] ? (<span className="text-green-400 font-semibold ps-2">Valid {name}</span>) : (<span className="text-red-400 font-semibold ps-2">Invalid {name}</span>) }))
-    //     return isValid[name];
-    // }
+        let message;
+
+        if (!value) {
+            message = `Enter the correct ${name.toLowerCase()}`;
+        } else if (isValid[name]) {
+            message = `Valid ${name}`;
+        } else {
+            message = `Invalid ${name}`;
+        }
+
+        setValidField((prev) => ({ ...prev, [name]: <span className={isValid[name] ? "text-green-400 font-semibold ps-2" : "text-red-400 font-semibold ps-2"}>{message}</span>}))
+        return isValid[name];
+    }
 
     const handleField = (e) => {
         const { name, value } = e.target;
         setInputField({ ...inputField, [name]: value })
-        // validError(name, value)
+        validError(name, value)
     }
 
-    const DataDelete = (id,e) => (setEmployeeStorage(employeeStorage.filter((data) => data.Id != id)))
-    const DataEdit = (data) =>( setInputField({Name : data.Name, Phone_number: data.Phone_number, Address: data.Address, Email: data.Email, Designation: data.Designation, Department: data.Department, City: data.City}),setIdMatch(data.Id))
+    const DataDelete = (id, e) => (setEmployeeStorage(employeeStorage.filter((data) => data.Id != id)))
+    const DataEdit = (data) => (setInputField({ Name: data.Name, Phone_number: data.Phone_number, Address: data.Address, Email: data.Email, Designation: data.Designation, Department: data.Department, City: data.City }), setIdMatch(data.Id))
     const RecSelect = (id) => {
         console.log("selected id", id);
     }
@@ -51,7 +61,8 @@ const Employee = () => {
     const FormSubmit = (e) => {
         e.preventDefault();
 
-        const isValidForm = Object.values(inputField).every(value => value.trim() !== '');
+        const isValidForm = Object.keys(inputField).every(name => validError(name, inputField[name]));
+        
         if (isValidForm) {
             const randomId = generateUniqueId({ // const randomId = Math.floor(Math.random() * 10000);
                 length: 4,
@@ -61,6 +72,7 @@ const Employee = () => {
             const dataToSubmit = { ...inputField, Id: IdMatch || randomId };
             setEmployeeStorage(prev => IdMatch ? prev.map((data) => data.Id === IdMatch ? dataToSubmit : data) : [...prev, dataToSubmit]);
             setInputField({ Name: '', Phone_number: '', Address: '', Email: '', Designation: '', Department: '', City: '' });
+            setValidField({})
         } else {
             alert("Form not submitted");
         }
@@ -77,12 +89,14 @@ const Employee = () => {
                     <div className="w-12/12 relative flex items-center justify-center flex-col bg-my-image bg-cover bg-top  py-5 rounded-lg overflow-hidden">
                         <div className="absolute inset-0 bg-black opacity-65"></div>
                         <div className="w-6/12 border-2 backdrop-blur-lg rounded-lg overflow-hidden">
-                            <FormInputCreate inputBox={inputField} inputHandle={handleField} /*inputValid={validField}*/ />
-                            <SubmitButton FormSubmited={FormSubmit} />
+                            <form action="" className="p-3">
+                                <FormInputCreate inputBox={inputField} inputHandle={handleField} inputValid={validField}/>
+                                <SubmitButton FormSubmited={FormSubmit} />
+                            </form>
                         </div>
                     </div>
                     <div className="w-12/12 mt-5">
-                        <DataShow EmployeeData={employeeStorage} DeleteFun={DataDelete} EditFun={DataEdit} SelectData = {RecSelect}/>
+                        <DataShow EmployeeData={employeeStorage} DeleteFun={DataDelete} EditFun={DataEdit} SelectData={RecSelect} />
                     </div>
                 </div >
             </section >
--- src/components/employeeform/forminput/FormInputCreate.jsx
+++ src/components/employeeform/forminput/FormInputCreate.jsx
@@ -1,53 +1,17 @@
-const FormInputCreate = ({ inputBox, inputHandle}) => {
-    const validData = (data) => {
-        if (!inputBox[data]) {
-            return null;
-        }
-
-        switch (data) {
-            case 'Name':
-                return /^[A-Za-z ]{2,}$/.test(inputBox[data]);
-            case 'Phone_number':
-                return /^[0-9]{10}$/.test(inputBox[data]);
-            case 'Address':
-                return /*/^[A-z -/]+\d+\s[A-z]+\s[A-z]+(.*)$/*/   /^[\d\sA-Za-z.,/-]{5,}$/.test(inputBox[data]);
-            case 'Email':
-                return /^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/.test(inputBox[data]);
-            case 'Designation':
-                return /^[A-Za-z ]{2,}$/.test(inputBox[data]);
-            case 'Department':
-                return /^[A-Za-z ]{2,}$/.test(inputBox[data]);
-            case 'City':
-                return /^[A-Za-z ]{2,}$/.test(inputBox[data]);
-            default:
-                return false;
-        }
-    }
-
+const FormInputCreate = ({ inputBox, inputHandle, inputValid}) => {
+    console.log("input valid",inputValid);
+    
     return (
         <>
-            <form action="" className="p-3">
-                {["Name", "Phone_number", "Address", "Email", "Designation", "Department", "City"].map((data) => {
-                    const isValid = validData(data);
-                    let Error;
+            {["Name", "Phone_number", "Address", "Email", "Designation", "Department", "City"].map((data) => {
 
-                    if (isValid === null) {
-                        Error = <span className="text-[#f97979] font-bold tracking-wider ps-2">Enter {data}</span>;
-                    } else if (isValid) {
-                        Error = <span className="text-green-600 font-bold tracking-wider ps-2">Valid {data}</span>;
-                    } else {
-                        Error = <span className="text-red-600 font-bold tracking-wider ps-2">Invalid {data}</span>;
-                    }
-
-                    return (
-                        <div className={`eployee-field ${data}`} key={data}>
-                            <label htmlFor="" className="font-semibold text-1xl mb-1 text-white">{data + " :-"}</label>{Error}
-                            <input type="text" placeholder={"Enter your " + data} name={data} value={inputBox[data]} className={`w-full border-2 py-2 px-3 bg-[#3e3c3c] rounded-lg mb-3 outline-none placeholder:text-[#a19d9d] text-white ${(isValid == 1 || isValid) ? "border-green-700" : "border-red-600"}`} onChange={inputHandle} />
-                        </div>
-                    )
-                })}
-
-            </form>
+                return (
+                    <div className={`eployee-field ${data}`} key={data}>
+                        <label htmlFor="" className="font-semibold text-1xl mb-1 text-white">{data + " :-"}</label>{inputValid[data]}
+                        <input type="text" placeholder={"Enter your " + data} name={data} value={inputBox[data]} className={`w-full border-2 py-2 px-3 bg-[#3e3c3c] rounded-lg mb-3 outline-none placeholder:text-[#a19d9d] text-white`} onChange={inputHandle} />
+                    </div>
+                )
+            })}
         </>
     )
 }
